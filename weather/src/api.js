a7baec9748c1e5a04ad5fca59e84598a3e3f8d9b
Noah Schutte <noahschutte@gmail.com>
2016-05-08 22:48:31 -0400

    retrieve data and console log weather info

--- weather/src/api.js
+++ weather/src/api.js
@@ -1,19 +1,19 @@
-ver rootUrl = 'api.openweathermap.org/data/2.5/weather?APPID=352ac572ad385d66b5a70ee89c39ffb4';
+var rootUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=352ac572ad385d66b5a70ee89c39ffb4';
 
 var kelvintoF = function(kelvin) {
   return Math.round((kelvin - 273.15) * 1.8 + 32) + ' ˚F'
 };
 
 module.exports = function(latitude, longitude) {
-  var url = `${rootUrl}$lat=${latitude}$lon=${longitude}`;
+  var url = `${rootUrl}&lat=${latitude}&lon=${longitude}`;
 
-  fetch(url)
+  return fetch(url)
     .then(function(response){
       return response.json()
     })
     .then(function(json){
       return {
-      city: json.city,
+      city: json.name,
       temperature: kelvintoF(json.main.temp),
       description: json.weather[0].description
       }
