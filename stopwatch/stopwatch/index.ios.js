3875c7594fc25352662a430c7a69bedbe5073e5a
Noah Schutte <noahschutte@gmail.com>
2016-05-08 20:12:59 -0400

    align items inside app

--- stopwatch/stopwatch/index.ios.js
+++ stopwatch/stopwatch/index.ios.js
@@ -1,5 +1,4 @@
 var React = require('react-native');
-
 var {
   Text,
   View,
@@ -7,23 +6,22 @@ var {
   StyleSheet
 } = React;
 
-
 var StopWatch = React.createClass({
   render: function() {
     return <View style={styles.container}>
-      <View style={styles.header}>
-        <View>
+      <View style={[styles.header, this.border('yellow')]}>
+        <View style={[styles.timerWrapper, this.border('red')]}>
           <Text>
             00:00.00
           </Text>
         </View>
-        <View>
+        <View style={[styles.buttonWrapper, this.border('green')]}>
           {this.startStopButton()}
           {this.lapButton()}
         </View>
       </View>
 
-      <View style={styles.footer}>
+      <View style={[styles.footer, this.border('blue')]}>
         <Text>
           I am a list of Laps
         </Text>
@@ -43,6 +41,12 @@ var StopWatch = React.createClass({
         Lap
       </Text>
     </View>
+  },
+  border: function(color){
+    return {
+      borderColor: color,
+      borderWidth: 4
+    }
   }
 });
 
@@ -56,6 +60,17 @@ var styles = StyleSheet.create({
   },
   footer: {
     flex: 1
+  },
+  timerWrapper: {
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonWrapper: {
+    flex: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
   }
 });
 
