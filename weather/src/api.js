55e2d979c99ebec9ce90f8c8406b10bf7f529ceb
Noah Schutte <noahschutte@gmail.com>
2016-05-08 22:36:58 -0400

    format json api module

--- weather/src/api.js
+++ weather/src/api.js
@@ -1,5 +1,21 @@
 ver rootUrl = 'api.openweathermap.org/data/2.5/weather?APPID=352ac572ad385d66b5a70ee89c39ffb4';
 
+var kelvintoF = function(kelvin) {
+  return Math.round((kelvin - 273.15) * 1.8 + 32) + ' ˚F'
+};
+
 module.exports = function(latitude, longitude) {
   var url = `${rootUrl}$lat=${latitude}$lon=${longitude}`;
+
+  fetch(url)
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(json){
+      return {
+      city: json.city,
+      temperature: kelvintoF(json.main.temp),
+      description: json.weather[0].description
+      }
+    });
 }
