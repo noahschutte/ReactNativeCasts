96de7ceeb62ee19d21a1ff0095c5f3e9c95fb955
Noah Schutte <noahschutte@gmail.com>
2016-05-08 21:41:55 -0400

    Style laps

--- stopwatch/stopwatch/index.ios.js
+++ stopwatch/stopwatch/index.ios.js
@@ -13,7 +13,8 @@ var StopWatch = React.createClass({
     return {
       timeElapsed: null,
       running: false,
-      startTime: null
+      startTime: null,
+      laps: []
     }
   },
   render: function() {
@@ -31,11 +32,21 @@ var StopWatch = React.createClass({
       </View>
 
       <View style={styles.footer}>
-        <Text>
-          I am a list of Laps
-        </Text>
+        {this.laps()}
       </View>
     </View>
+  },
+  laps: function() {
+    return this.state.laps.map(function(time, index) {
+      return <View style={styles.lap}>
+        <Text style={styles.lapText}>
+          Lap #{index + 1}
+        </Text>
+        <Text style={styles.lapText}>
+          {formatTime(time)}
+        </Text>
+      </View>
+    });
   },
   startStopButton: function() {
     var style = this.state.running ? styles.stopButton : styles.startButton;
@@ -65,7 +76,8 @@ var StopWatch = React.createClass({
     var lap = this.state.timeElapsed;
 
     this.setState({
-      startTime: new Date()
+      startTime: new Date(),
+      laps: this.state.laps.concat([lap])
     });
   },
   handleStartPress: function() {
@@ -123,6 +135,13 @@ var styles = StyleSheet.create({
   },
   stopButton: {
     borderColor: 'red'
+  },
+  lap: {
+    justifyContent: 'space-around',
+    flexDirection: 'row'
+  },
+  lapText: {
+    fontSize: 30
   }
 });
 
